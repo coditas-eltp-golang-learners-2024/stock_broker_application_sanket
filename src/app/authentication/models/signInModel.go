e86d9540fc941d6878de344b6870a87d77106912
sanket <sanket.chaudhary@coditas.com>
2024-04-12 11:51:17 +0530

    SignIn File Deleted from SignUp

--- src/app/authentication/models/signInModel.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type UserCredentials struct {
-	Email    string `json:"email" validate:"required"`
-	Password string `json:"password" validate:"required"`
-}
-var UerCredentialsList []UserCredentials
